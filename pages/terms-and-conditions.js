e3e875d2126879ff1411f8f05c7d007643c0ce69
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2021-11-23 11:05:09 +0500

    linter error fixed

--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Container, Row, Col, CardColumns } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const TermsAndConditions = () => (
   <>
